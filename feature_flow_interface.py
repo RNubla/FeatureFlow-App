1e1af1d89a56050912fe483c5e3d2b21f0e4c6d4
RNubla <rnubla97@gmail.com>
2020-09-03 18:40:35 -0400

    added ui interfaces root of project

---
+++ feature_flow_interface.py
@@ -0,0 +1,159 @@
+# %%
+import os
+import shutil
+import time
+# from os import pipe
+from pathlib import Path
+
+# from numpy.core.arrayprint import str_format
+# from sequence_run import main, getInterpolationIndex, getInterpolationRange, getIteration
+from sequence_runner import main, getInterpolationIndex, getInterpolationRange, getIteration
+import cv2
+
+ffmpeg_exe = Path().cwd() / 'ffmpeg.exe'
+
+
+class FrameRate:
+    def getInitialFPS(videoName) -> float:
+        videoCap = cv2.VideoCapture(videoName)
+        fps: float = videoCap.get(cv2.CAP_PROP_FPS)
+        return float(fps)
+
+
+class CheckResolution:
+    def __init__(self, file_name):
+        self.file = file_name
+        self.vcap = cv2.VideoCapture(self.file)
+
+    def getWidth(self):
+        width = self.vcap.get(cv2.CAP_PROP_FRAME_WIDTH)
+        return int(width)
+
+    def getHeight(self):
+        height = self.vcap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        return int(height)
+
+
+class InformationGetters:
+    def iterationCount() -> int:
+        return getIteration()
+
+    def interpolationIndex() -> int:
+        return int(getInterpolationIndex())
+
+    def interpolationRange() -> int:
+        return int(getInterpolationRange())
+
+
+class VideoDecimation:
+    def __init__(self, input_path_from_gui: str, output_path_from_gui: str):
+        self. current_working_dir: str = Path.cwd()
+        self.input_video: str = input_path_from_gui
+        self.output_path: str = output_path_from_gui
+
+    def removeDuplicateFrames(self):
+        filename = self.dir_name_based_off_filename
+        file = self.path_to_dir_name_based_off_filename / filename
+        print(file)
+        print('ffmpeg.exe  -qscale 0 -i ' + str(self.file_name) +
+              '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' + str(self.input_dir / filename) + '-decimated.mp4')
+        os.system('ffmpeg.exe -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' +
+                  str(self.input_dir / filename) + '-decimated.mp4')
+        os.system(str(ffmpeg_exe) + ' -i ' + str(file) + '.mp4 -vsync 0 -frame_pts true -vf mpdecimate ' +
+                  str(self.input_dir / filename) + '-decimated.mp4')
+
+
+class Resolution360p:
+    def __init__(self, file_input_path_from_gui: str, interpolation: int, output_path_from_gui: str):
+        self.current_working_dir: str = Path.cwd()
+        self.input_video: str = file_input_path_from_gui
+        self.video_name: str = os.path.splitext(os.path.basename(file_input_path_from_gui))[
+            0]   # returns file name from example.mp4 to example
+        self.interp_num: int = interpolation
+        self.output_path: str = output_path_from_gui
+
+        self.finished_file_name: str = Path.cwd() / str(self.video_name + '-final.mp4')
+        self.interp_output_file_name: str = Path.cwd() / 'output.mp4'
+
+    def runFeatureFlow(self):
+        print(self.interp_num)
+        print('Input Video: ', (self.input_video))
+
+        main(self.interp_num, self.input_video)
+
+        print(Path(self.interp_output_file_name) / Path(self.output_path))
+        time.sleep(3)
+        shutil.move('output.mp4', str(Path(self.output_path) /
+                                      '{}-final.mp4'.format(self.video_name)))
+
+    def deleteFiles(self):
+        cwd = Path(self.current_working_dir)
+        filename = self.dir_name_based_off_filename
+        cwd_decimated_file = cwd / str(filename + '-decimated.mp4')
+        # decimated_file = str(Path(self.input_dir / filename)) + '-decimated.mp4'
+        print(cwd_decimated_file)
+        os.remove(cwd_decimated_file)
+
+
+class Resolution720p:
+    def __init__(self, file_input_path_from_gui: str, interpolation: int, output_path_from_gui: str):
+        self.current_working_dir: str = Path.cwd()
+        self.input_video: str = file_input_path_from_gui
+        self.video_name: str = os.path.splitext(os.path.basename(self.input_video))[
+            0]   # returns file name from example.mp4 to example
+        self.interp_num: int = interpolation
+        self.output_path: str = output_path_from_gui
+
+        self.finished_file_name: str = Path.cwd() / str(self.video_name + '-final.mp4')
+        self.interp_output_file_name: str = Path.cwd() / 'output.mp4'
+
+    def splitVideoIntoSections(self):
+        print('Video Name: ', self.video_name)
+        input_split_dict = {
+            'top-left': [self.input_video, "crop=iw/2:ih/2:0:0", str(Path(self.output_path) / '{}-top-left.mp4').format(self.video_name)],
+            'top-right': [self.input_video, "crop=iw/2:ih/2:ow:0", str(Path(self.output_path) / '{}-top-right.mp4').format(self.video_name)],
+            'bottom-left': [self.input_video, "crop=iw/2:ih/2:0:oh", str(Path(self.output_path) / '{}-bottom-left.mp4').format(self.video_name)],
+            'bottom-right': [self.input_video, "crop=iw/2:ih/2:ow:oh", str(Path(self.output_path) / '{}-bottom-right.mp4').format(self.video_name)],
+        }
+
+        for v in input_split_dict.items():
+            # print(v[1][0])
+            # print(v[1][1])
+            print(str(ffmpeg_exe) + ' -i ' +
+                  v[1][0] + ' -vsync 0 -frame_pts true -vf '+'"' + v[1][1] + '"' + ' ' + v[1][2])
+            os.system(str(ffmpeg_exe) + ' -i ' +
+                      v[1][0] + ' -vsync 0 -frame_pts true -vf '+'"' + v[1][1] + '"' + ' ' + v[1][2])
+
+    def runFeatureFlow(self):
+        input_split_files = [str(Path(self.output_path) / '{}-top-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-top-right.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-bottom-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-bottom-right.mp4'.format(self.video_name))
+                             ]
+
+        for video in input_split_files:
+            print(video)
+            main(self.interp_num, video)
+            shutil.move('output.mp4', video)
+
+    def stitchVideo(self):
+        input_split_files = [str(Path(self.output_path) / '{}-top-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-top-right.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-bottom-left.mp4'.format(self.video_name)),
+                             str(Path(self.output_path) /
+                                 '{}-bottom-right.mp4'.format(self.video_name))
+                             ]
+        os.system(str(ffmpeg_exe) +
+                  ' -i ' + input_split_files[0] +
+                  ' -i ' + input_split_files[1] +
+                  ' -i ' + input_split_files[2] +
+                  ' -i ' + input_split_files[3] +
+                  ' -filter_complex "[0:v][1:v]hstack=inputs=2[top];[2:v][3:v]hstack=inputs=2[bottom];[top][bottom]vstack=inputs=2[v]" -map "[v]" ' +
+                  str(Path(self.output_path) / '{}-final.mp4'.format(self.video_name)))
+        for video in input_split_files:
+            os.remove(video)
--- feflow_ui.py
+++ feflow_ui.py
@@ -4,167 +4,179 @@ import wx.adv
 from pathlib import Path
 import threading
 
-from ThirdParty.FeatureFlow.feature_flow_interface import CheckResolution, Resolution720p, Resolution360p, InformationGetters, FrameRate
+from feature_flow_interface import CheckResolution, Resolution720p, Resolution360p, InformationGetters, FrameRate
+
 
 class FeFlowApp(wx.App):
-	def OnInit(self):
-		self.xrcFile : str = str(Path.cwd() / 'formbuilder' / 'noname.xrc')
-		self.res = xrc.XmlResource(self.xrcFile)
-		self.init_frame()
-		return True
+    def OnInit(self):
+        self.xrcFile: str = str(Path.cwd() / 'formbuilder' / 'noname.xrc')
+        self.res = xrc.XmlResource(self.xrcFile)
+        self.init_frame()
+        return True
 
-	def init_frame(self):
-		self.interpNum: int = 2
-		self.inputFile : str = ''
-		self.outputPath : str = ''
-		self.FPS : float = 0.0
-		self.newFPS : float = 0.0
-		self.condition = threading.Condition()
+    def init_frame(self):
+        self.interpNum: int = 2
+        self.inputFile: str = ''
+        self.outputPath: str = ''
+        self.FPS: float = 0.0
+        self.newFPS: float = 0.0
+        self.condition = threading.Condition()
 
-		self.mainFrame = self.res.LoadFrame(None, 'mainFrame')
-		self.mainMenuBar = xrc.XRCCTRL(self.mainFrame, 'mainMenuBar')
+        self.mainFrame = self.res.LoadFrame(None, 'mainFrame')
+        self.mainMenuBar = xrc.XRCCTRL(self.mainFrame, 'mainMenuBar')
 
-		self.quitFromMenuBar = xrc.XRCCTRL(self.mainFrame, 'exitMenuItem')
-		self.mainFrame.Bind(wx.EVT_MENU, self.onQuit, id=xrc.XRCID('exitMenuItem'))
+        self.quitFromMenuBar = xrc.XRCCTRL(self.mainFrame, 'exitMenuItem')
+        self.mainFrame.Bind(wx.EVT_MENU, self.onQuit,
+                            id=xrc.XRCID('exitMenuItem'))
 
-		self.ogFPS = xrc.XRCCTRL(self.mainFrame, 'ogFPSTextCtrl')
-		self.mainFrame.Bind(wx.EVT_BUTTON, self.onSelectFile, id=xrc.XRCID('ogFPSTextCtrl'))
+        self.ogFPS = xrc.XRCCTRL(self.mainFrame, 'ogFPSTextCtrl')
+        self.mainFrame.Bind(wx.EVT_BUTTON, self.onSelectFile,
+                            id=xrc.XRCID('ogFPSTextCtrl'))
 
-		self.interpFPS = xrc.XRCCTRL(self.mainFrame, 'interpFPSTextCtrl')
-		self.mainFrame.Bind(wx.EVT_CHOICE, self.getChoice, id=xrc.XRCID('interpFPSTextCtrl'))
+        self.interpFPS = xrc.XRCCTRL(self.mainFrame, 'interpFPSTextCtrl')
+        self.mainFrame.Bind(wx.EVT_CHOICE, self.getChoice,
+                            id=xrc.XRCID('interpFPSTextCtrl'))
 
-		self.choice = xrc.XRCCTRL(self.mainFrame, 'interpolationChoice')
-		self.mainFrame.Bind(wx.EVT_CHOICE, self.getChoice, id=xrc.XRCID('interpolationChoice'))
+        self.choice = xrc.XRCCTRL(self.mainFrame, 'interpolationChoice')
+        self.mainFrame.Bind(wx.EVT_CHOICE, self.getChoice,
+                            id=xrc.XRCID('interpolationChoice'))
 
-		self.videoPicker = xrc.XRCCTRL(self.mainFrame, 'videoFilePicker')
-		self.mainFrame.Bind(wx.EVT_FILEPICKER_CHANGED, self.onSelectFile, id=xrc.XRCID('videoFilePicker'))
+        self.videoPicker = xrc.XRCCTRL(self.mainFrame, 'videoFilePicker')
+        self.mainFrame.Bind(wx.EVT_FILEPICKER_CHANGED,
+                            self.onSelectFile, id=xrc.XRCID('videoFilePicker'))
 
-		self.outputPicker = xrc.XRCCTRL(self.mainFrame, 'outputDirPicker')
-		self.mainFrame.Bind(wx.EVT_DIRPICKER_CHANGED, self.onSelectOutputDir, id=xrc.XRCID('outputDirPicker'))
+        self.outputPicker = xrc.XRCCTRL(self.mainFrame, 'outputDirPicker')
+        self.mainFrame.Bind(wx.EVT_DIRPICKER_CHANGED,
+                            self.onSelectOutputDir, id=xrc.XRCID('outputDirPicker'))
 
-		self.runBtn = xrc.XRCCTRL(self.mainFrame, 'RunBtn')
-		self.mainFrame.Bind(wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('RunBtn'))
-		
-		# self.inProgressDialog = xrc.XRCCTRL(self.mainFrame, 'progressDialog')
-		# self.mainFrame.Bind(wx.EVT_BUTTON, self.showProgressBar, id=xrc.XRCID('progressDialog'))
+        self.runBtn = xrc.XRCCTRL(self.mainFrame, 'RunBtn')
+        self.mainFrame.Bind(
+            wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('RunBtn'))
 
-		self.statusBar = xrc.XRCCTRL(self.mainFrame, 'statusBar')
-		self.mainFrame.Bind(wx.EVT_TEXT, self.showIterationCount, id=xrc.XRCID('statusBar'))
+        # self.inProgressDialog = xrc.XRCCTRL(self.mainFrame, 'progressDialog')
+        # self.mainFrame.Bind(wx.EVT_BUTTON, self.showProgressBar, id=xrc.XRCID('progressDialog'))
 
-		self.pbar = xrc.XRCCTRL(self.mainFrame, 'progressBar')
-		# self.mainFrame.Bind(wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('progressBar'))
-		self.mainFrame.Bind(wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('progressBar'))
+        self.statusBar = xrc.XRCCTRL(self.mainFrame, 'statusBar')
+        self.mainFrame.Bind(
+            wx.EVT_TEXT, self.showIterationCount, id=xrc.XRCID('statusBar'))
 
+        self.pbar = xrc.XRCCTRL(self.mainFrame, 'progressBar')
+        # self.mainFrame.Bind(wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('progressBar'))
+        self.mainFrame.Bind(
+            wx.EVT_BUTTON, self.applicationThreads, id=xrc.XRCID('progressBar'))
 
-		self.mainFrame.Show()
+        self.mainFrame.Show()
 
-	def onQuit(self, event):
-		self.mainFrame.Close()
+    def onQuit(self, event):
+        self.mainFrame.Close()
 
-	def getChoice(self, event):
-		choice = self.choice.GetStringSelection()
-		self.choice.GetCurrentSelection()
-		if choice == '2x':
-			self.interpFPS.SetValue('')
-			print('Interpolation Number: ', choice)
-			self.interpNum = 2
-			self.newFPS = float(self.FPS * self.interpNum)
-			self.interpFPS.write(str(self.newFPS))
-		elif choice == '4x':
-			self.interpFPS.SetValue('')
-			print('Interpolation Number: ', choice)
-			self.interpNum = 4
-			self.newFPS = float(self.FPS * self.interpNum)
-			self.interpFPS.write(str(self.newFPS))
-		elif choice == '8x':
-			self.interpFPS.SetValue('')
-			print('Interpolation Number: ', choice)
-			self.interpNum = 8
-			self.newFPS = float(self.FPS * self.interpNum)
-			self.interpFPS.write(str(self.newFPS))
-		# print(self.interpNum)
-	
-	def onSelectFile(self, event):
-		file = self.videoPicker.GetPath()
-		self.inputFile = file
-		
-		fps = FrameRate.getInitialFPS(self.inputFile)
-		self.FPS = fps
-		self.ogFPS.SetValue(str(self.FPS))
-		print('INPUT FILE : ',self.inputFile)
+    def getChoice(self, event):
+        choice = self.choice.GetStringSelection()
+        self.choice.GetCurrentSelection()
+        if choice == '2x':
+            self.interpFPS.SetValue('')
+            print('Interpolation Number: ', choice)
+            self.interpNum = 2
+            self.newFPS = float(self.FPS * self.interpNum)
+            self.interpFPS.write(str(self.newFPS))
+        elif choice == '4x':
+            self.interpFPS.SetValue('')
+            print('Interpolation Number: ', choice)
+            self.interpNum = 4
+            self.newFPS = float(self.FPS * self.interpNum)
+            self.interpFPS.write(str(self.newFPS))
+        elif choice == '8x':
+            self.interpFPS.SetValue('')
+            print('Interpolation Number: ', choice)
+            self.interpNum = 8
+            self.newFPS = float(self.FPS * self.interpNum)
+            self.interpFPS.write(str(self.newFPS))
+        # print(self.interpNum)
 
-	def onSelectOutputDir(self, event):
-		path = self.outputPicker.GetPath()
-		self.outputPath = path
-		print('Output Folder: ', self.outputPath)
+    def onSelectFile(self, event):
+        file = self.videoPicker.GetPath()
+        self.inputFile = file
 
+        fps = FrameRate.getInitialFPS(self.inputFile)
+        self.FPS = fps
+        self.ogFPS.SetValue(str(self.FPS))
+        print('INPUT FILE : ', self.inputFile)
 
-	def completeDialog(self):
-		wx.MessageBox('Feature Flow has finished interpolating your video. \n Please check your output directory', 'Interpolation Complete', wx.OK |wx.ICON_EXCLAMATION)
-	
-	def missingFileDialog(self):
-		wx.MessageBox('You have not selected an Input Video or and Output Directory. \n Please specify them', 'Error', wx.OK |wx.ICON_ERROR)
+    def onSelectOutputDir(self, event):
+        path = self.outputPicker.GetPath()
+        self.outputPath = path
+        print('Output Folder: ', self.outputPath)
 
-	def onRunFeatureFlow(self, event):
-		# index = InformationGetters.interpolationIndex()
-		# range = InformationGetters.interpolationRange()
-		# print(range)
-		if self.inputFile != '' and self.outputPath != '':
-			resolution = CheckResolution(self.inputFile)
-			if resolution.getWidth() < int(1280) and resolution.getHeight() < int(720):
-				interpolate360 = Resolution360p(self.inputFile, self.interpNum, self.outputPath)
-				interpolate360.runFeatureFlow()
-				self.completeDialog()
-			else:
-				interpolate720 = Resolution720p(self.inputFile, self.interpNum, self.outputPath)
-				interpolate720.splitVideoIntoSections()
-				interpolate720.runFeatureFlow()
-				interpolate720.stitchVideo()
-				self.completeDialog
-		else:
-			self.missingFileDialog()
+    def completeDialog(self):
+        wx.MessageBox('Feature Flow has finished interpolating your video. \n Please check your output directory',
+                      'Interpolation Complete', wx.OK | wx.ICON_EXCLAMATION)
 
-	def showProgressBar(self, cond):
-		print('Starting showProgressBar Thread')
-		t = threading.currentThread()
-		with cond:
-			cond.wait()
-			print('Resource is availabe to showProgressBar')
-			self.pbar.SetRange(InformationGetters.interpolationRange())
-			self.pbar.SetValue(InformationGetters.interpolationIndex())
-			print('Range: ',InformationGetters.interpolationRange())
-			# wx.ProgressDialog('Interpolation In Progress', 'Interpolating your video', maximum=100)
+    def missingFileDialog(self):
+        wx.MessageBox('You have not selected an Input Video or and Output Directory. \n Please specify them',
+                      'Error', wx.OK | wx.ICON_ERROR)
 
-	def fflowProducer(self, cond):
-		print('Starting Feature Flow Producer Thread')
-		with cond:
-			print('Making resource availale for showProgressBar')
-			# self.onRunFeatureFlow
-			cond.notifyAll()
+    def onRunFeatureFlow(self, event):
+        # index = InformationGetters.interpolationIndex()
+        # range = InformationGetters.interpolationRange()
+        # print(range)
+        if self.inputFile != '' and self.outputPath != '':
+            resolution = CheckResolution(self.inputFile)
+            if resolution.getWidth() < int(1280) and resolution.getHeight() < int(720):
+                interpolate360 = Resolution360p(
+                    self.inputFile, self.interpNum, self.outputPath)
+                interpolate360.runFeatureFlow()
+                self.completeDialog()
+            else:
+                interpolate720 = Resolution720p(
+                    self.inputFile, self.interpNum, self.outputPath)
+                interpolate720.splitVideoIntoSections()
+                interpolate720.runFeatureFlow()
+                interpolate720.stitchVideo()
+                self.completeDialog
+        else:
+            self.missingFileDialog()
 
-		# print('Range: ', InformationGetters.interpolationRange())
-		# print('Index: ', InformationGetters.interpolationIndex())
-		# progressDialog = wx.ProgressDialog('Interpolation In Progress', 'Interpolating your video', maximum=InformationGetters.interpolationRange)
-		# progressDialog.Update(InformationGetters.interpolationIndex)
+    def showProgressBar(self, cond):
+        print('Starting showProgressBar Thread')
+        t = threading.currentThread()
+        with cond:
+            cond.wait()
+            print('Resource is availabe to showProgressBar')
+            self.pbar.SetRange(InformationGetters.interpolationRange())
+            self.pbar.SetValue(InformationGetters.interpolationIndex())
+            print('Range: ', InformationGetters.interpolationRange())
+            # wx.ProgressDialog('Interpolation In Progress', 'Interpolating your video', maximum=100)
 
+    def fflowProducer(self, cond):
+        print('Starting Feature Flow Producer Thread')
+        with cond:
+            print('Making resource availale for showProgressBar')
+            # self.onRunFeatureFlow
+            cond.notifyAll()
 
-	def applicationThreads(self, event):
-		# pbar = threading.Thread(name='Progress Bar', target=self.showProgressBar, args=(self.condition,))
-		# fflow = threading.Thread(name='FeatureFlow Producer', target=self.onRunFeatureFlow, args=(self.condition,))
-		# pbar.start()
-		# fflow.start()
-		runFeatureFlow = threading.Thread(target=self.onRunFeatureFlow, args=(event,))
-		# progressBar = threading.Thread(target=self.showProgressBar, args=(event,))
-		runFeatureFlow.start()
-		# progressBar.start()
-		# progressBar.join()
-		# runFeatureFlow.join()
+        # print('Range: ', InformationGetters.interpolationRange())
+        # print('Index: ', InformationGetters.interpolationIndex())
+        # progressDialog = wx.ProgressDialog('Interpolation In Progress', 'Interpolating your video', maximum=InformationGetters.interpolationRange)
+        # progressDialog.Update(InformationGetters.interpolationIndex)
 
-	def showIterationCount(self):
-		iteration = InformationGetters.iterationCount()
-		self.statusBar.SetStatusText('Iteration: ', str(iteration))
+    def applicationThreads(self, event):
+        # pbar = threading.Thread(name='Progress Bar', target=self.showProgressBar, args=(self.condition,))
+        # fflow = threading.Thread(name='FeatureFlow Producer', target=self.onRunFeatureFlow, args=(self.condition,))
+        # pbar.start()
+        # fflow.start()
+        runFeatureFlow = threading.Thread(
+            target=self.onRunFeatureFlow, args=(event,))
+        # progressBar = threading.Thread(target=self.showProgressBar, args=(event,))
+        runFeatureFlow.start()
+        # progressBar.start()
+        # progressBar.join()
+        # runFeatureFlow.join()
+
+    def showIterationCount(self):
+        iteration = InformationGetters.iterationCount()
+        self.statusBar.SetStatusText('Iteration: ', str(iteration))
 
 
 if __name__ == "__main__":
-	app = FeFlowApp(False)
-	app.MainLoop()
+    app = FeFlowApp(False)
+    app.MainLoop()
